7bb195023fc73e9a963026610ab2e423ac338ba9
yulin <2247586191@qq.com>
2022-11-10 00:44:05 +0800

    2022年11月10日 00:44:11

--- src/main.js
+++ src/main.js
@@ -4,18 +4,18 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 // vue工具库引入
-import store from "@/store";
 import router from "@/router";
+import store from "@/store";
 
 // 全局组件引入
-import TypeNav from "@/components/TypeNav";
 import Carousel from "@/components/Carousel";
+import TypeNav from "@/components/TypeNav";
 // *全局组件注册
 Vue.component(TypeNav.name, TypeNav);
 Vue.component(Carousel.name, Carousel);
 
 // *饿了么-ui
-import { MessageBox, Button, Message, Pagination } from "element-ui";
+import { Button, Message, MessageBox, Pagination } from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 // 两种引入方式
 // Vue.component(Button.name, Button);
@@ -28,12 +28,12 @@ Vue.prototype.$alert = MessageBox.alert;
 
 // 全局插件引入
 // *vue-awesome-swiper依赖于swiper，所以不用引入swiper
-import VueAwesomeSwiper from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
+import VueAwesomeSwiper from "vue-awesome-swiper";
 Vue.use(VueAwesomeSwiper);
 // 引入懒加载图片
-import VueLazyload from 'vue-lazyload'
-import loading from '@/assets/images/giphy.gif'
+import loading from '@/assets/images/giphy.gif';
+import VueLazyload from 'vue-lazyload';
 Vue.use(VueLazyload, {
   // preLoad: 1.3,
   // error: 'dist/error.png',
@@ -52,11 +52,11 @@ export default new Vue({
   router,
   store,
   beforeCreate() {
-    (Vue.prototype.$bus = this),
-      // 全局 API 总线
-      (Vue.prototype.$API = API);
+    Vue.prototype.$bus = this;
+    // 全局 API 总线
+    Vue.prototype.$API = API;
   },
   mounted() {
-    console.log(this)
+    // console.log(this);
   }
 }).$mount("#app");
